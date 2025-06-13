a594a62c31a10af28198a15dfa232907774e554d
theo-code1 <codingbusiness99@gmail.com>
2025-06-13 20:29:25 +0100

    pushing files into github

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return(
+    <>
+      <h1 className="text-4xl  ">hello world </h1>
+      <h1 class="font-Inter text-4xl">hello world </h1>
+    </>
+  )
+}
+export default App;
