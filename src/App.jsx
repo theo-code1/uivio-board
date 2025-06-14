9766a50208781f405682fb8c39c9d79426157158
theo-code1 <codingbusiness99@gmail.com>
2025-06-14 10:12:57 +0100

    complete the Structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 const App = () => {
   return(
     <>
-      <h1 className="text-4xl  ">hello world </h1>
-      <h1 class="font-Inter text-4xl">hello world </h1>
+      <h1 className="text-4xl font-semibold ">hello world </h1>
+      <h1 class="font-inter-medium font-medium text-4xl">hello world </h1>
+      <h1 class="font-inter-bold font-bold text-4xl">hello world </h1>
+      <h1 class="font-inter- font-bold text-4xl">hello world </h1>
+      <h1 class="font-inter-regular font-regular text-4xl">hello world </h1>
     </>
   )
 }
