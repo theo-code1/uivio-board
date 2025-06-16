c20af333d16f4f92a0bdfeb224037738f9eadeec
theo-code1 <codingbusiness99@gmail.com>
2025-06-16 10:18:59 +0100

    Update Design to Workflow Cards

--- src/components/DesignCard.jsx
+++ src/components/DesignCard.jsx
@@ -1,11 +1,11 @@
 
-function DesignCard({Label, Headline, WorkImg }){
+function DesignCard({Label, Headline, WorkImg, Color, ImgClassName }){
     
     return(
         <article className="w-[70%] bg-white font-inter-regular p-20 pt-12 mx-auto rounded-4xl flex flex-col gap-4">
-            <h3 className="label font-semibold text-green-500 text-center">{Label}</h3>
+            <h3 className={`label font-semibold text-${Color}-500 text-center`}>{Label}</h3>
             <h1 className='text-center text-5xl font-inter-semibold font-semibold '>{Headline}  </h1>
-            <img src={WorkImg} alt="Clipboard History" className='mx-auto' draggable='false'/>
+            <img src={WorkImg} alt="Clipboard History" className={`mx-auto ${ImgClassName}`} draggable='false'/>
 
         </article>
     )
--- src/components/DesignToWorkflow.jsx
+++ src/components/DesignToWorkflow.jsx
@@ -9,18 +9,30 @@ function DesignToWorkflow() {
     const CardsInfo = [
         {
             Label: 'KEEP',
-            Headline: 'Unlimited clipboard history',
-            Img: {ClipboardHistory}
-        }
+            Headline: `Unlimited clipboard history`,
+            Img: ClipboardHistory
+        },
+        {
+            Label: 'SEARCH',
+            Headline: 'Find anything you copies on any device',
+            Img: SearchUtility
+        },
+        {
+            Label: 'ORGANIZE',
+            Headline: 'Quickly access what you need frequently',
+            Img: OrganizeAccess
+        },
     ]
 
     return(
-        <section className="bg-[#F5F5F7] max-w-screen h-[250dvh] font-inter-regular pt-48 flex flex-col gap-44">
+        <section className="bg-[#F5F5F7] max-w-screen h-auto pb-32 font-inter-regular pt-48 flex flex-col gap-44">
             <h1 className="design-headline text-7xl font-inter-bold font-bold text-center leading-20">
             Unobtrusive design that <br />blends into your workflow
             </h1>
-            <div className="Cards gap-8">
-                <DesignCard />
+            <div className="Cards flex flex-col gap-6 ">
+                <DesignCard Label={CardsInfo[0].Label} Color={'green'} Headline={CardsInfo[0].Headline} WorkImg={CardsInfo[0].Img}/>
+                <DesignCard Label={CardsInfo[1].Label} Color={'green'} Headline={CardsInfo[1].Headline} WorkImg={CardsInfo[1].Img} ImgClassName={`border `}/>
+                <DesignCard Label={CardsInfo[2].Label} Color={'green'} Headline={CardsInfo[2].Headline} WorkImg={CardsInfo[2].Img}/>
             </div>
         </section>
     )
