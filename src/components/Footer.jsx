c175ad1b908cf48de446ed556a2cb02f0af7b31c
theo-code1 <codingbusiness99@gmail.com>
2025-06-28 17:38:39 +0100

    solve git problem

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,17 @@ const Footer = () => {
     const ListItemClassName = 'text-[16px] font-normal mb-2 cursor-pointer text-white/60 hover:text-white/80 transition-all duration-150 w-fit';
 
   return (
+<<<<<<< HEAD
     <footer className='h-[40dvh] max-w-screen flex flex-col items-center py-12 text-white bg-black'>
         <div className="top-rows flex gap-24 text-start w-fit">
             <img src={UIvioWhiteLogo} alt="UIvio white Logo" className='h-14 opacity-70'/>
             <ul className="2nd-row">
+=======
+    <footer className='max-w-screen flex flex-col items-center pt-12 pb-8 px-8 text-white bg-black'>
+        <div className="top-rows flex justify-center flex-wrap md:flex-nowrap gap-8 md:gap-24 text-start w-fit">
+            <img src={UIvioWhiteLogo} alt="UIvio white Logo" className='h-14 opacity-70 order-1'/>
+            <ul className="2nd-row order-2 mb-4">
+>>>>>>> 797b158 (made the footer responsive)
                 <li className={ListHeaderClassName}>Paste</li>
                 <li className={ListItemClassName}>Try for free</li>
                 <li className={ListItemClassName}>Get on Setapp</li>
@@ -18,14 +25,14 @@ const Footer = () => {
                 <li className={ListItemClassName}>Updates</li>
                 <li className={ListItemClassName}>Pricing</li>
             </ul>
-            <ul className="3rd-row">
+            <ul className="3rd-row order-3 mb-4">
                 <li className={ListHeaderClassName}>Resources</li>
                 <li className={ListItemClassName}>Help</li>
                 <li className={ListItemClassName}>Blog</li>
                 <li className={ListItemClassName}>Terms of use</li>
                 <li className={ListItemClassName}>Privacy policy</li>
             </ul>
-            <ul className="4th-row">
+            <ul className="4th-row order-4 mb-4">
                 <li className={ListHeaderClassName}>More</li>
                 <li className={ListItemClassName}>About</li>
                 <li className={ListItemClassName}>Contact</li>
@@ -33,14 +40,22 @@ const Footer = () => {
                 <li className={ListItemClassName}>Suggest a feature</li>
                 <li className={ListItemClassName}>Jobs</li>
             </ul>
-            <div className="5th-row">
+            <div className="5th-row order-5 mb-4">
                 <h3 className={ListHeaderClassName}>Follow us</h3>
                 <div className="social-media flex gap-4">
                     <img src={Twitter} alt="X Logo" className='opacity-70 hover:opacity-85 transition-all duration-150'/>
                     <img src={Facebook} alt="Facebook Logo" className='opacity-70 hover:opacity-85 transition-all duration-150'/>
                 </div>
             </div>
+<<<<<<< HEAD
 
+=======
+        </div>
+        <hr className='my-8 bg-white/10 w-4/5 lg:w-[50%]'/>
+        <div className="copyrigh text-sm text-white/70 flex justify-between gap-2 lg:ap-48 w-screen lg:w-7/10 px-4 ">
+            <span>@ 2025 Paste Team ApS, All right reserved.</span>
+            <span>Alll trademarks are the property of their respective owners.</span>
+>>>>>>> 797b158 (made the footer responsive)
         </div>
     </footer>
   )
